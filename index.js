ed55d890177fee9f2c73b4d796cfd0cd9a70de44
lipzie27 <rohmath816@gmail.com>
2026-03-25 18:46:51 +0700

    Implement basic Mineflayer bot functionality
    config

---
+++ index.js
@@ -0,0 +1,27 @@
+const mineflayer = require('mineflayer');
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'nowscraft.mcsh.io', // Ganti dengan IP servermu
+        port: 2022,                // Ganti jika portnya berbeda
+        username: 'Btbylipzie'      // Nama bot di dalam game
+    });
+
+    bot.on('login', () => {
+        console.log('bat berhasil masuk ke server!');
+    });
+
+    bot.on('chat', (username, message) => {
+        if (username === bot.username) return;
+        bot.chat('Saya ai buatan asnoo/lipzie.');
+    });
+
+    // Menangani error dan kick agar bot otomatis login ulang
+    bot.on('error', (err) => console.log('Error:', err));
+    bot.on('end', () => {
+        console.log('Bot terputus, mencoba login kembali dalam 5 detik...');
+        setTimeout(createBot, 5000);
+    });
+}
+
+createBot();
